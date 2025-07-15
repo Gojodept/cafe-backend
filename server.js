1553dc90a370b2a53103cd2f7c6a791f06d5c856
gojodept <arindamkashyap625@gmail.com>
2025-07-15 14:48:11 +0530

    updated

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
-import productRouter from "./routes/productRoute.js";
+//import productRouter from "./routes/productRoute.js";
 import cors from "cors";
 dotenv.config();
 const app = express();
