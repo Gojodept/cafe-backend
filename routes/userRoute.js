5f65e6d766a06ca4ebc64c0caef59a1771dd1c88
gojodept <arindamkashyap625@gmail.com>
2025-07-15 14:07:01 +0530

    upadated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,3 +1,26 @@
+// import express from "express";
+// import { authenticate, authorize } from "../middlewares/auth.js";
+// const Router = express.Router();
+// import {
+//   register,
+//   login,
+//   profile,
+//   updateUser,
+//   deleteUser,
+//   showUsers,
+//   updateProfile,
+//   getUser,
+// } from "../controllers/userController.js";
+// Router.post("/register", register);
+// Router.post("/login", login);
+// Router.get("/", authenticate, authorize("admin"),showUsers);// 
+// Router.get("/:id", authenticate, authorize("admin"), getUser);
+// Router.patch("/:id", authenticate, authorize("admin"), updateUser);
+// Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
+// Router.get("/:id/profile", authenticate, profile);
+// Router.patch("/:id/profile", authenticate, updateProfile);
+
+// export default Router;
 import express from "express";
 import { authenticate, authorize } from "../middlewares/auth.js";
 const Router = express.Router();
@@ -10,14 +33,21 @@ import {
   showUsers,
   updateProfile,
   getUser,
-} from "../controllers/userControllers.js";
+  addUser,
+} from "../controllers/userController.js";
+
+//user routes
 Router.post("/register", register);
 Router.post("/login", login);
-Router.get("/", authenticate, authorize("admin"), showUsers);
-Router.get("/:id", authenticate, authorize("admin"), getUser);
-Router.patch("/:id", authenticate, authorize("admin"), updateUser);
-Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
-Router.get("/:id/profile", authenticate, profile);
-Router.patch("/:id/profile", authenticate, updateProfile);
+Router.get("/:id/profile", profile);
+Router.patch("/:id/profile", updateProfile);
+
+//admin routes
+Router.get("/", showUsers);
+Router.post("/", addUser);
+Router.get("/:id", getUser);
+Router.patch("/:id", updateUser);
+Router.delete("/:id", deleteUser);
+
 
 export default Router;
--- server.js
+++ server.js
@@ -1,27 +1,58 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import userRouter from "./routes/userRoute.js"; //userRouter = Router what ever is being exported from userRouter Is imported in userRouter can also write Router in pace of userRouter that will also work fine
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// dotenv.config();
+// const dbuser = encodeURIComponent(process.env.DBUSER);
+// const dbpass = encodeURIComponent(process.env.DBPASS);
+// // mongoose
+// //   .connect(
+// //     `mongodb+srv://${dbuser}:${dbpass}@cluster0.p3ndcui.mongodb.net/cafe?retryWrites=true&w=majority&appName=Cluster0`
+// //   )
+// mongoose.connect(`mongodb://localhost:27017/cafe`)
+//   .then(() => {
+//     console.log("✅ MongoDB connected");
+//     app.listen(8080, () => {
+//       console.log("🚀 Server started on port 8080");
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("❌ MongoDB connection error:", err);
+//   });
+
+// app.use("/api/users", userRouter);
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+//import productRouter from "./routes/productRoute.js";
 import cors from "cors";
-import userRouter from "./routes/userRoute.js"; //userRouter = Router what ever is being exported from userRouter Is imported in userRouter can also write Router in pace of userRouter that will also work fine
-const app = express();
-app.use(express.json());
-app.use(cors());
 dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
 mongoose
   .connect(
-    `mongodb+srv://${dbuser}:${dbpass}@cluster0.p3ndcui.mongodb.net/cafe?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
   )
-
   .then(() => {
-    console.log("✅ MongoDB connected");
     app.listen(8080, () => {
-      console.log("🚀 Server started on port 8080");
+      console.log("Server started");
     });
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
   });
 
 app.use("/api/users", userRouter);
+//app.use("/api/products", productRouter);
